b7655511202bfcaaa3ab33bf2d627b4a39987458
alejandroyunes <alejandroyunes@outlook.com>
2024-12-01 18:13:45 -0500

    added turewell

---
+++ data/counter/turewell610.ts
@@ -0,0 +1,168 @@
+import type { ProductDetails } from '~/types/product-details'
+
+const turewell610: ProductDetails = {
+  id: 6,
+  fileName: 'turewell610',
+  info: {
+    image: 'https://res.cloudinary.com/dkv2cmxfs/image/upload/v1700560366/Best/Bitvae/cou9qve0gvhthybqligf.webp',
+    imageSmall: 'https://res.cloudinary.com/dkv2cmxfs/image/upload/v1700560366/Best/Bitvae/ditfkdygrm4qoohqzttw.webp',
+    alt: "Bitvae C2 Professional Water Dental Flosser, 260ml",
+    title: "Bitvae Water Dental Flosser for Teeth - Black",
+    subtitle: "Cordless, 3 Modes, 5 Intensities, IPX7 Waterproof, 260ml Reservoir",
+    description: "The Bitvae C2 provides advanced oral care with customizable cleaning options, a large-capacity reservoir, and a sleek design suitable for travel.",
+    price: "$19.99",
+    updatedDate: '03/12/2023',
+    buyLink: 'https://amzn.to/3R3veFS',
+    cta: 'Buy Now on Amazon',
+    rating: {
+      stars: 4.4,
+      reviews: 16372,
+    },
+    characteristics: [
+      "Wide Mouth Reservoir for Easy Cleaning.",
+      "260ml tank for a 60-second full-mouth clean.",
+      "3 Modes and 5 Intensity Settings for personalized oral care.",
+      "FDA, FCC, CE, PSE certified for safety.",
+      "40-day battery life after 4 hours of charging.",
+    ]
+  },
+  tableOfContent: [
+    { id: '#the-best-of-bitvae-c2', content: '\u2022 The Best of Bitvae C2' },
+    { id: '#why-bitvae-c2', content: '\u2022 Why Choose Bitvae C2?' },
+    { id: '#how-does-the-bitvae-c2-compares-to-other-irrigators', content: '\u2022 Comparison table with other irrigators' },
+    { id: '#pro-and-cons-bitvae-c2', content: '\u2022 Pros and Cons' },
+  ],
+  title: {
+    id: 'the-best-of-bitvae-c2',
+    title: 'The Best of Bitvae C2',
+    description: "Portable, with a 300ml tank, ergonomic design, 6 nozzles, and special pressure frequency for a brighter smile.",
+    subtext: 'Achieve a brighter smile with advanced features and ergonomic design.',
+  },
+  features: {
+    id: 'why-bitvae-c2',
+    title: 'Why Choose Bitvae C2?',
+    data: [
+      {
+        title: 'Ultimate Portability',
+        description: 'Compact and cordless design makes it perfect for use at home, in the office, or on the go.',
+      },
+      {
+        title: 'Precision Cleaning',
+        description: 'Advanced 3 modes and 5 intensity settings target blind spots and hard-to-reach areas for a tailored experience.',
+      },
+      {
+        title: 'Hygienic Wide-Mouth Reservoir',
+        description: 'Detachable, easy-to-clean design ensures optimal hygiene for everyday use.',
+      },
+      {
+        title: 'Versatile Nozzle Options',
+        description: 'Includes 6 replaceable nozzles for diverse cleaning needs, from gum care to plaque removal.',
+      },
+      {
+        title: 'Long-Lasting Battery',
+        description: 'Enjoy up to 40 days of usage on a single 4-hour charge, perfect for worry-free travel.',
+      },
+      {
+        title: 'Large Capacity Reservoir',
+        description: 'The 260ml water tank supports a full 60-second deep clean without frequent refills.',
+      },
+      {
+        title: 'Certified Quality',
+        description: 'FDA, FCC, CE, and PSE certifications guarantee reliable safety and performance.',
+      },
+      {
+        title: 'Enhanced Oral Health',
+        description: 'Promotes gum health, fresh breath, and bright smiles with regular use.',
+      },
+    ],
+  },
+  comparison: {
+    id: 'how-does-the-bitvae-c2-compares-to-other-irrigators',
+    title: 'Compare the Bitvae C2 with other irrigators',
+    description: 'In this table, you can see all the analyzed models, their features, notable advantages, user ratings, and the results of our analyses in 2024.',
+    models: [
+      {
+        id: 1,
+        productUrl: '/reviews/portable-and-affordable-oral-irrigators/bitvaeC2',
+        model: 'Bitvae C2',
+        image: 'https://res.cloudinary.com/dkv2cmxfs/image/upload/v1700560366/Best/Bitvae/ditfkdygrm4qoohqzttw.webp',
+        alt: 'Bitvae C2 Oral Irrigator Image',
+        prime: true,
+        dimensions: '2.6 x 3.7 x 8.78 inches; 13.44 ounces',
+        tips: '6',
+        capacity: '260 ml',
+        highlight: 'Effective Cleaning, Compact Design, Long Battery Life',
+        problem: 'Water Resistance, Durability',
+        price: 'Great Price',
+        satisfaction: '9/10',
+        view: 'View Price',
+      },
+      {
+        id: 2,
+        productUrl: '/reviews/portable-and-affordable-oral-irrigators/coslusF5020E',
+        model: 'Coslus F5020E',
+        image: 'https://res.cloudinary.com/dkv2cmxfs/image/upload/v1700541237/Best/Coslus300ml/bekj7waajyb9egnw08a8.webp',
+        alt: 'Coslus F5020E Oral Irrigator',
+        prime: true,
+        dimensions: '1.97 x 2.74 x 12.2 in, 15.52 oz',
+        tips: '5',
+        capacity: '300ml',
+        highlight: 'Dual-thread stream, Child mode, IPX7 waterproof',
+        problem: 'Limited water capacity',
+        price: 'Excelent price',
+        satisfaction: '9/10',
+        view: 'View Price',
+      },
+      {
+        id: 3,
+        productUrl: '/reviews/portable-and-affordable-oral-irrigators/waterpikF02',
+        model: 'Waterpik WF-02',
+        image: 'https://res.cloudinary.com/dkv2cmxfs/image/upload/v1700500931/Best/WaterPicWF02/hpp82kmw3yjoyjbira9h.webp',
+        alt: 'Waterpik Model WF-02',
+        prime: true,
+        dimensions: '3.6 x 2.5 x 11.75 in, 11.68 oz',
+        tips: '2',
+        capacity: '5oz reservoir for 30 seconds of flossing time',
+        highlight: 'Economical, Portable, Customizable.',
+        problem: 'Battery replacement, Limited water capacity.',
+        price: 'Best buy',
+        satisfaction: '9/10',
+        view: 'View Price',
+      },
+      {
+        id: 4,
+        productUrl: '/reviews/portable-and-affordable-oral-irrigators/nicwellF5025',
+        model: 'Nicwell F5025',
+        image: 'https://res.cloudinary.com/dkv2cmxfs/image/upload/v1700448959/Best/Nicwellf5025/ofjladptkdqgsbyeqcve.webp',
+        alt: 'Nicwell 200ml Oral Irrigator',
+        prime: true,
+        dimensions: '8.03 x 4.02 x 2.73 in, 14 oz',
+        tips: '5',
+        capacity: '200 ml',
+        highlight: 'Cleaning Orthodontic Gums, Prevent Bad Breath',
+        problem: 'Charging',
+        price: 'Great Price',
+        satisfaction: '9/10',
+        view: 'View Price',
+      },
+    ]
+  },
+  proAndCons: {
+    id: 'pro-and-cons-bitvae-c2',
+    title: "Pros and Cons",
+    description: "Bitvae C2 offers impressive cleaning capabilities and portability, though durability and water resistance may be considerations.",
+    cta: 'Buy on Amazon',
+    buyLink: 'https://amzn.to/3R3veFS',
+    pros: [
+      { title: 'Thorough cleaning with customizable modes and nozzles.' },
+      { title: 'Portable and lightweight for travel.' },
+      { title: 'Durable battery lasting up to 40 days.' },
+    ],
+    cons: [
+      { title: 'Durability concerns from some customers.' },
+      { title: 'Potential water leakage issues.' },
+    ],
+  },
+}
+
+export default turewell610
