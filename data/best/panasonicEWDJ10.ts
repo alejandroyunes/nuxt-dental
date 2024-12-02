d3e9e3f98a9827fe029cc439c203aac95e1a251a
alejandroyunes <alejandroyunes@outlook.com>
2024-12-02 00:20:59 -0500

    Create panasonicEWDJ10.ts

---
+++ data/best/panasonicEWDJ10.ts
@@ -0,0 +1,120 @@
+import type { ProductDetails } from '~/types/product-details'
+
+const panasonicEWDJ10: ProductDetails = {
+  id: 4,
+  fileName: 'panasonicEWDJ10',
+  info: {
+    image: "",
+    imageSmall: "https://res.cloudinary.com/dkv2cmxfs/image/upload/v1701541960/typesOfIrrigators/portable/Panasonic-EW-DJ10/k2z9vwzvovgs6rfzzemh.webp",
+    alt: "",
+    title: "",
+    subtitle: "",
+    description: "",
+    price: "",
+    updatedDate: "",
+    buyLink: "",
+    cta: "Buy on Amazon",
+    rating: {
+      stars: 0,
+      reviews: 0,
+    },
+    characteristics: [
+      "",
+      "",
+      "",
+      "",
+      "",
+    ]
+  },
+  tableOfContent: [
+    { id: "title", content: "Overview" },
+    { id: "features", content: "Key Features" },
+    { id: "comparison", content: "Comparison with Similar Models" },
+    { id: "pros-cons", content: "Pros and Cons" },
+  ],
+  title: {
+    id: "",
+    title: "",
+    description: "",
+    subtext: "",
+  },
+  features: {
+    id: "",
+    title: "",
+    data: [
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+    ],
+  },
+  comparison: {
+    id: "",
+    title: "",
+    description: "",
+    models: [
+      {
+        id: 0,
+        productUrl: "/reviews/portable-oral-irrigators/best/panasonicEWDJ10",
+        model: "",
+        image: "https://res.cloudinary.com/dkv2cmxfs/image/upload/v1701541960/typesOfIrrigators/portable/Panasonic-EW-DJ10/k2z9vwzvovgs6rfzzemh.webp",
+        alt: "Panasonic Portable Water Flosser EW-DJ10",
+        prime: false,
+        dimensions: "",
+        tips: "",
+        capacity: "",
+        highlight: "",
+        problem: "",
+        price: "",
+        satisfaction: "",
+        view: "",
+      }
+    ]
+  },
+  proAndCons: {
+    id: "",
+    title: "",
+    description: "",
+    cta: "Buy on Amazon",
+    buyLink: "",
+    pros: [
+      { title: "" },
+      { title: "" },
+      { title: "" },
+    ],
+    cons: [
+      { title: "" },
+      { title: "" },
+    ],
+  },
+}
+
+export default panasonicEWDJ10
